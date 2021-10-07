89308b6dd674741b9c02e5e77b1e366e526ae377
Fas96 <firibu.anyass@gmail.com>
2021-10-08 04:03:32 +0900

    member update

--- querys/member.sql
+++ querys/member.sql
@@ -1,7 +1,8 @@
 CREATE TABLE MEMBER
 (
     member_id  VARCHAR(255) primary key  UNIQUE,
-    member_password VARCHAR(255),
+    member_password VARCHAR(255) not null,
+    member_name VARCHAR(64) not null ,
     member_isRestricted SMALLINT DEFAULT(0),
     member_isAdmin SMALLINT DEFAULT(0)
 )
